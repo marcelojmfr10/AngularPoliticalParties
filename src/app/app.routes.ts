a79e3d098a21e87633c387f8449b1fa800ff60c1
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-03-26 21:22:32 -0600

    Estructura básica de proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ChartPage } from './live-chart/pages/chart-page/chart-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ChartPage,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ src/app/live-chart/pages/chart-page/chart-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-page',
+  imports: [],
+  templateUrl: './chart-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChartPage {}
